9dce574c611ba84c1947498bf53970c465e35c73
Felipe O Ribeiro <felipeoliveirasl@hotmail.com>
2022-06-18 09:27:45 -0300

    chore: force sync

--- js/banner.js
+++ js/banner.js
@@ -23,6 +23,11 @@ const banners = [
 
 function initBanner(selector, banners) {
   let index = 0;
+  const elements = {
+    banners: [],
+    details: [],
+    commands: [],
+  };
   const parent = document.getElementById(selector);
   const container = document.createElement("div");
   container.className = "banners";
@@ -35,20 +40,49 @@ function initBanner(selector, banners) {
     const banner = banners[i];
     const img = document.createElement("img");
     img.hidden = i > 0;
-    img.src = banner.img_url;
+    img.src = `/images/${banner.img_url}`;
 
     const detail = document.createElement("div");
     detail.className = "detail";
+    detail.hidden = i > 0;
     detail.append(banner.info);
 
     container.append(img, detail);
 
     const command = document.createElement("span");
     command.className = "command-item";
+    command.onclick = function () {
+        go(i)
+    }
+
     commands.append(command);
 
     if (i === 0) {
       command.classList.add("active");
     }
+
+    elements.banners.push(img);
+    elements.details.push(detail);
+    elements.commands.push(command);
+  }
+
+  function go(to) {
+    const currentBanner = elements.banners[index];
+    const currentDetail = elements.details[index];
+    const currentCommander = elements.commands[index];
+    currentBanner.hidden = true;
+    currentDetail.hidden = true;
+    currentCommander.classList.remove('active')
+
+    index = to
+
+    const nextBanner = elements.banners[index];
+    const nextDetail = elements.details[index];
+    const nextCommander = elements.commands[index];
+    nextBanner.hidden = false;
+    nextDetail.hidden = false;
+    nextCommander.classList.add('active')
   }
 }
+
+initBanner("banner-content", banners);
